0530fe5bc3d877a566e1a6790b320ab30ee2e2d9
Emil Magnusson <Candyynnz@gmail.com>
2023-01-24 14:57:10 +0100

    Setup Python Web server.

---
+++ staticHandler.py
@@ -0,0 +1,12 @@
+from response.requestHandler import RequestHandler
+
+class StaticHandler(RequestHandler):
+    def __init__(self):
+      return
+
+    def find(self, file_path):
+      return
+
+    def setContentType(self, ext):
+      return
+
