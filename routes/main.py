d0d6e9c30e4663e976703ac3f90ee2616d98cd7d
Henrik <henrik.eklund01@hotmail.com>
2023-01-31 13:00:44 +0100

    uppdatera alla sidor och login

--- routes/main.py
+++ routes/main.py
@@ -6,5 +6,17 @@ routes = {
     "/index": {
         "template": "index.html",
         "post": "indexPost.py"
+    },
+    "/shop": {
+              "template": "Shop.html",
+              "post": "indexPost.py"
+    },
+    "/login": {
+              "template": "login.html",
+              "post": "indexPost.py"
+    },
+    "/checkout": {
+              "template": "checkout.html",
+              "post": "indexPost.py"
     }
 }
